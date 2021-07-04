087f0d2dc8e68211a92fe13e364e58f1c2552024
mdmartinvega <mdmartinvega@gmail.com>
2021-07-04 17:16:00 +0200

    Fetch para la página ProfilePage con los datos de cada usuario

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,18 +1,49 @@
 import '../styles/ProfilePage.css';
+import {Link, useParams} from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 export default function Profile_page() {
 
     const PHOTO_PERSON = "http://placeimg.com/370/250/people";
 
+    const {id} = useParams();
+    const API_PROFILES = `http://localhost:8000/api/buddies/${id}`;
+
+    const [descriptionUser, setDescriptionUser] = useState({});
+
+    useEffect(() => {
+        fetch(API_PROFILES)
+        .then(response => response.json())
+        .then(data => setDescriptionUser(data));  
+        }, []);
+
     //TODO: Hacer un fetch()-GET a un endpoint PROFILE-DETAILS
     //TMDBAPI/FILMS/1225214 <- el ID lo capturas por useParams()
     return (
-
+        
         <div className="profile-page">
             <div className="details-profile">
             </div>
             <div className="image-profile">
                 <img src={PHOTO_PERSON} alt="" />
+            </div>
+            <div className="description-cardProfile">
+                <p className="name">{`${descriptionUser.name}, ${descriptionUser.age}`}</p>
+                <p className="city">{descriptionUser.city}</p>
+                <div className="languages">
+                    {descriptionUser.languages?.map(language => {
+                        return <div> {`${language}`} </div>
+                    })
+                    }
+                </div>
+                <div className="interests">
+                    {descriptionUser.interests?.map(interest => {
+                        return <div> {`${interest}`} </div>
+                    })
+                    }
+                </div>
+                <p className="other_Details">{descriptionUser.bio}</p>
+                <Link to={`/ProfilePage/${descriptionUser.id}`} className="btn">Contacta conmigo</Link>
             </div>
 
         </div>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,25 +1,32 @@
 import '../styles/Register.css';
 import { Link, useParams } from 'react-router-dom';
 import { useForm } from '../hooks/useForm';
-import { useEffect } from 'react';
+import { useState } from 'react';
 
 export default function Register({setContacts}) {
 
     let { buddy } = useParams();
 
-    const initialFormState = {name: "", lastname: "", email: "", checked: `${buddy="become-buddy"}`};
+    const initialFormState = {name: "", lastname: "", email: "", checked: `${buddy="become-buddy"}`, };
     const [form, handleInputChange] = useForm(initialFormState); // Custom Hook
 
-    const handleSubmit = e => {
+    const API_PROFILES = 'http://localhost:8000/api/buddies';
+
+    const[addUser, setAddUser] = useState({});
+
+    function handleSubmit(e) {
         e.preventDefault();
-        setContacts(contacts => [...contacts, form])
-    };
 
-   
+        fetch(API_PROFILES, {
+            method: "POST",
+            body: JSON.stringify(),
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+        })
+        .then(response => response.json())
+        .then(json => console.log(json));
 
-    useEffect(() => {
- 
-    }, [])
+        setAddUser({});
+    }
 
     return (
         <div className="register-form">
@@ -29,21 +36,13 @@ export default function Register({setContacts}) {
                 <form onSubmit={handleSubmit}>
                 <div className="choose-option">
                         <div>
-                            <input onChange={handleInputChange} type="radio" name="userType" id="User" checked={buddy !== "become-buddy" ? true : false} />
+                            <input onChange={handleInputChange} type="radio" name="userType" id="User"/>
                             <label for="User">Busco un buddy</label>
                         </div>                                     
                         <div>
-                            <input onChange={handleInputChange} type="radio" name="UserType" id="Buddy" checked={buddy === "be-buddy" ? true : false}/>
+                            <input onChange={handleInputChange} type="radio" name="UserType" id="Buddy"/>
                             <label for="Buddy">Quiero ser un buddy</label>
                         </div>
-                </div>
-                <div className="select-options">
-                    <select name="city-move" id="cityInput" disabled>
-                        <option value={form.cityUser}>¿Dónde te mudas?</option>
-                    </select>
-                    <select name="city-live" id="cityInput" disabled>
-                        <option value={form.cityBuddy}>¿Dónde vives?</option>
-                    </select>
                 </div>
                 <div className="inputs-form">
                     <label htmlFor="name">Nombre</label>
@@ -53,7 +52,74 @@ export default function Register({setContacts}) {
                     <label htmlFor="lastName">Apellidos</label>
                     <input onChange={handleInputChange} value={form.lastName} name="lastName" type="text" id="lastNameInput" placeholder="Apellidos" required/>
                 </div>
+                <div className="select-options">
+                    <select name="city-move" id="cityInput" disabled>
+                        <option value={form.cityUser}>Selecciona la ciudad en la que vives o a la que te mudas</option>
+                    </select>
+                </div>
+                <fieldset>
+                    <legend>¿Qué idiomas hablas?</legend>
+                        <div className="languages">
+                            <div>
+                                <input type="checkbox" name="Spanish" id="Spanish" />
+                                <label for="Spanish">Español</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" name="English" id="English" />
+                                <label for="English">Inglés</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" name="French" id="French" />
+                                <label for="French">Francés</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" name="Portuguese" id="Portuguese" />
+                                <label for="Portuguese">Portugués</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" name="Italian" id="Italian" />
+                                <label for="Italian">Italiano</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" name="German" id="German" />
+                                <label for="German">Alemán</label>
+                            </div>
+                        </div>
+                </fieldset>
 
+                <fieldset>
+                    <legend>¿Qué buscas o qué puedes ofrecer?</legend>
+                        <div className="languages">
+                            <div>
+                                <input type="checkbox" name="routesInput" id="routes"/>
+                                <label for="routes">Rutas</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" name="proceduresInput" id="procedures"/>
+                                <label for="procedures">Trámites</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" name="workInput" id="work"/>
+                                <label for="work">Trabajo</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" name="cultureInput" id="culture"/>
+                                <label for="culture">Cultura</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" name="politicInput" id="politic"/>
+                                <label for="politic">Política</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" name="meetPeopleInput" id="meetPeople"/>
+                                <label for="meetPeople">Conocer gente</label>
+                            </div>
+                            <div>
+                                <input type="checkbox" name="languagesInput" id="languages"/>
+                                <label for="languages">Idiomas</label>
+                            </div>
+                        </div>
+                </fieldset>
                 <div className="inputs-form">
                     <label htmlFor="EmailInput">Email</label>
                     <input onChange={handleInputChange} value={form.email} name="email" type="email" id="EmailInput" placeholder="tuemail@tuemail.com" required/>
@@ -68,7 +134,7 @@ export default function Register({setContacts}) {
                 </div> */}
                 {/* TODO: Averiguar como insertar el campo vuelva a introducir contraseña y sus atributos */}
 
-                <button type="submit" value="Log in">Accede</button>
+                <button onSubmit = {handleSubmit} type="submit" value="Log in">Accede</button>
                 </form>
                 <div>
                 <Link to="/login">¿Ya tienes una cuenta con nosotros?</Link>
