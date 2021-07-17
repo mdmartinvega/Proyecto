2e89fd1781fe2dbeaaa834de7987334def6cc16d
mdmartinvega <mdmartinvega@gmail.com>
2021-07-17 19:30:36 +0200

    Hecha redirección al editar datos

--- src/components/ConfigurationDashboard.js
+++ src/components/ConfigurationDashboard.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import '../styles/ConfigurationDashboard.css';
+import { useHistory } from 'react-router-dom';
 import { useForm } from '../hooks/useForm';
+
 
 
 export default function ConfigurationDashboard({user}) {
@@ -11,6 +13,8 @@ export default function ConfigurationDashboard({user}) {
     
     const initialFormState = {name: user?.name, lastName: user?.lastName, email: user?.email, password: user?.password, age: user?.age, bio: user?.bio};
     const [form, handleInputChange] = useForm(initialFormState, languagesList, interestsList); // Custom Hook
+    const history = useHistory();
+
 
     async function handleSubmit(e){
         e.preventDefault();
@@ -24,54 +28,54 @@ export default function ConfigurationDashboard({user}) {
         const response = await fetch(`http://localhost:8000/api/buddies/${user.id}`, options);
         const data = await response.json();
         console.log(data);
+
+        // TODO: Verificar porque sale del login
+        if(response.status >= 200 && response.status < 300) {
+            alert("Tus datos se han modificado correctamente");
+            history.push("/dashboard")
+        } 
     }
 
+    
     return (
         <div className="main-dashboard">
-                <div className="register-form">
+            <div className="register-form">
                 <h2>Modifica tu perfil</h2>
-                <form onSubmit={handleSubmit}>
-                <div className="inputs-form">
-                    <label htmlFor="name">Nombre</label>
-                    <input onChange={handleInputChange} value={form.name} name="name" type="text" id="nameInput" placeholder="Nombre"/>
-                </div>
-                <div className="inputs-form">
-                    <label htmlFor="lastName">Apellidos</label>
-                    <input onChange={handleInputChange} value={form.lastName} name="lastName" type="text" id="lastNameInput" placeholder="Apellidos"/>
-                </div>
-                <div className="inputs-form">
-                <label htmlFor="age">¿Cuántos años tienes?</label>
-                    <input onChange={handleInputChange} type="number" id="age" name="age" value={form.age} min="0" max="120" placeholder="Escoge"/>
-                </div>
-                <div class="inputs-form">
-                    <label htmlFor="bio">Háblanos sobre tí</label>
-                    <br />
-                    <textarea onChange={handleInputChange} type="password" id="bio" name="bio" value={form.bio} placeholder="Escriba aquí su texto"
-                        ></textarea>
-                </div>
-                <div className="inputs-form">
-                    <label htmlFor="EmailInput">Email</label>
-                    <input onChange={handleInputChange} value={form.email} name="email" type="email" id="EmailInput" placeholder="tuemail@tuemail.com"/>
-                </div>
-                <div className="inputs-form">
-                    <label htmlFor="PasswordInput">Contraseña</label>
-                    <input onChange={handleInputChange} value={form.password} name="password" type="password" id="PasswordInput" placeholder="***************"/>
-                </div>
-                <div className="inputs-form">
-                    <label htmlFor="PasswordInput">Vuelve a introducir tu contraseña</label>
-                    <input onChange={handleInputChange} value={form.passwordRepeat} name="password" type="password" id="PasswordInput" placeholder="***************"/>
-                </div>
-                {/* TODO: Averiguar como insertar el campo vuelva a introducir contraseña y sus atributos */}
-                {/* TODO: Agregar link que lleve a la página de login */}
-                <button type="submit" value="Enviar">Enviar</button>
+                <form >
+                    <div className="inputs-form">
+                        <label htmlFor="name">Nombre</label>
+                        <input onChange={handleInputChange} value={form.name} name="name" type="text" id="nameInput" placeholder="Nombre"/>
+                    </div>
+                    <div className="inputs-form">
+                        <label htmlFor="lastName">Apellidos</label>
+                        <input onChange={handleInputChange} value={form.lastName} name="lastName" type="text" id="lastNameInput" placeholder="Apellidos"/>
+                    </div>
+                    <div className="inputs-form">
+                    <label htmlFor="age">¿Cuántos años tienes?</label>
+                        <input onChange={handleInputChange} type="number" id="age" name="age" value={form.age} min="0" max="120" placeholder="Escoge"/>
+                    </div>
+                    <div class="inputs-form">
+                        <label htmlFor="bio">Háblanos sobre tí</label>
+                        <br />
+                        <textarea onChange={handleInputChange} type="password" id="bio" name="bio" value={form.bio} placeholder="Escriba aquí su texto"
+                            ></textarea>
+                    </div>
+                    <div className="inputs-form">
+                        <label htmlFor="EmailInput">Email</label>
+                        <input onChange={handleInputChange} value={form.email} name="email" type="email" id="EmailInput" placeholder="tuemail@tuemail.com"/>
+                    </div>
+                    <div className="inputs-form">
+                        <label htmlFor="PasswordInput">Contraseña</label>
+                        <input onChange={handleInputChange} value={form.password} name="password" type="password" id="PasswordInput" placeholder="***************"/>
+                    </div>
+                    <div className="inputs-form">
+                        <label htmlFor="PasswordInput">Vuelve a introducir tu contraseña</label>
+                        <input onChange={handleInputChange} value={form.passwordRepeat} name="password" type="password" id="PasswordInput" placeholder="***************"/>
+                    </div>
+                    {/* TODO: Averiguar como insertar el campo vuelva a introducir contraseña y sus atributos */}
+                    {/* TODO: Agregar link que lleve a la página de login */}
+                    <button onSubmit={handleSubmit} type="submit" value="Enviar">Enviar</button>
                 </form>
-                <div>
-
-                    
-                
-          
-                
-            </div>
             </div>
         </div>
     )
--- src/components/DeleteAccount.js
+++ src/components/DeleteAccount.js
@@ -1,11 +1,25 @@
 import '../styles/DeleteAccount.css';
 
-export default function DeleteAccount() {
+export default function DeleteAccount({user}) {
 
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        const options = {
+            method: "DELETE",
+            headers: {"Content-type": "application/json"},
+            body: JSON.stringify(user)
+        }
+
+        const response = await fetch(`http://localhost:8000/api/messages/${user.id}`, options);
+        const data = await response.json();
+        console.log(data);
+   
+    };
     
     return (
         <div className="delete-account">
-            <button /*onSubmit = {handleSubmit}*/ type="submit" value="Eliminar cuenta">Eliminar cuenta</button>
+            <button onSubmit = {handleSubmit} type="submit" value="Eliminar cuenta">Eliminar cuenta</button>
         </div>
     )
 }
